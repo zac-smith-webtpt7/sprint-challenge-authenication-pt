7b314100dae1c4c85d27fdf196ec24d55ded0685
Zac Smith <zrs3141592@gmail.com>
2019-12-16 20:01:14 -0700

    fix testing, forgot "test": "cross-env DB_ENV=testing jest --watch"

---
+++ api/server.spec.js
@@ -0,0 +1,8 @@
+const server = require('./server.js')
+const request = require('supertest')
+
+describe('GET /', , () => {
+  it('has process.env.DB_ENV as "testing"', () => {
+    expect(process.env.DB_ENV).toBe('testing')
+  })
+})
